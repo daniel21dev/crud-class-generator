108302df9cd114f67680d88c4453f9fee0a3c957
daniel <daniel21dev@gmail.com>
2022-06-24 22:01:53 -0500

    feat: crud generator

--- packages/generator/src/generator.ts
+++ packages/generator/src/generator.ts
@@ -2,6 +2,7 @@ import { generatorHandler, GeneratorOptions } from '@prisma/generator-helper'
 import { logger } from '@prisma/sdk'
 import path from 'path'
 import { GENERATOR_NAME } from './constants'
+import { genClass } from './helpers/genClass'
 import { genEnum } from './helpers/genEnum'
 import { writeFileSafely } from './utils/writeFileSafely'
 
@@ -17,15 +18,31 @@ generatorHandler({
     }
   },
   onGenerate: async (options: GeneratorOptions) => {
+    // generates enums
     options.dmmf.datamodel.enums.forEach(async (enumInfo) => {
       const tsEnum = genEnum(enumInfo)
-
       const writeLocation = path.join(
         options.generator.output?.value!,
         `${enumInfo.name}.ts`,
       )
-
       await writeFileSafely(writeLocation, tsEnum)
     })
+    // generate crud clasess
+    //const models = options.dmmf.schema.inputObjectTypes.prisma
+
+    options.dmmf.datamodel.models.forEach(async (model) => {
+      const crudClass = genClass(model.name)
+      await writeFileSafely(
+        path.join(options.generator.output?.value!, `${model.name}Crud.ts`),
+        crudClass,
+      )
+    })
+
+    /*
+    const inputs = options.dmmf.schema.inputObjectTypes.prisma
+      .map((x) => JSON.stringify(x.name))
+      .join(', ')
+    const out = `const models = [${models}];
+      const inputs = [${inputs}]`*/
   },
 })
---
+++ packages/generator/src/helpers/genClass.ts
@@ -0,0 +1,34 @@
+export const genClass = (modelName: string): string => {
+  const modelNameLower = modelName.toLowerCase()
+  return `
+    import { Prisma, PrismaClient } from '@prisma/client';
+
+    export class ${modelName}Crud{
+
+        constructor(private prisma:PrismaClient){}
+        
+        find(args: Prisma.${modelName}FindManyArgs){
+          return this.prisma.${modelNameLower}.findMany(args);
+        }
+
+        findOne(where: Prisma.${modelName}WhereUniqueInput){
+          return this.prisma.${modelNameLower}.findUnique({ where });
+        }
+
+        save(data: Prisma.${modelName}CreateInput){
+          return this.prisma.${modelNameLower}.create({ data });
+        }
+      
+        update(where: Prisma.${modelName}WhereUniqueInput, data: Prisma.${modelName}UpdateInput){
+          return this.prisma.${modelNameLower}.update({
+            where,
+            data,
+          });
+        }
+      
+        delete(where: Prisma.${modelName}WhereUniqueInput){
+          return this.prisma.${modelNameLower}.delete({ where });
+        }
+    }
+    `
+}
