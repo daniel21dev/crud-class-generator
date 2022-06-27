cd4f6d293e795120de18f3dec48857f42b62e21f
daniel <daniel21dev@gmail.com>
2022-06-26 20:33:05 -0500

    refactor: removes genEnum feat

--- packages/generator/src/generator.ts
+++ packages/generator/src/generator.ts
@@ -3,7 +3,6 @@ import { logger } from '@prisma/sdk'
 import path from 'path'
 import { GENERATOR_NAME } from './constants'
 import { genClass } from './helpers/genClass'
-import { genEnum } from './helpers/genEnum'
 import { writeFileSafely } from './utils/writeFileSafely'
 
 const { version } = require('../package.json')
@@ -18,18 +17,7 @@ generatorHandler({
     }
   },
   onGenerate: async (options: GeneratorOptions) => {
-    // generates enums
-    options.dmmf.datamodel.enums.forEach(async (enumInfo) => {
-      const tsEnum = genEnum(enumInfo)
-      const writeLocation = path.join(
-        options.generator.output?.value!,
-        `${enumInfo.name}.ts`,
-      )
-      await writeFileSafely(writeLocation, tsEnum)
-    })
     // generate crud clasess
-    //const models = options.dmmf.schema.inputObjectTypes.prisma
-
     options.dmmf.datamodel.models.forEach(async (model) => {
       const crudClass = genClass(model.name)
       await writeFileSafely(
@@ -37,12 +25,5 @@ generatorHandler({
         crudClass,
       )
     })
-
-    /*
-    const inputs = options.dmmf.schema.inputObjectTypes.prisma
-      .map((x) => JSON.stringify(x.name))
-      .join(', ')
-    const out = `const models = [${models}];
-      const inputs = [${inputs}]`*/
   },
 })
